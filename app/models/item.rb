3337c5839478d12d21ed4697e221ba54c4bd0d9a
James Dabbs <jamesdabbs@gmail.com>
2016-06-22 10:10:34 -0400

    Validate due_date in the future

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,13 @@ class Item < ActiveRecord::Base
   validates_presence_of :list, :description
   validates_length_of :description, minimum: 2
   validates_uniqueness_of :description, scope: :list
+  validate :due_date_cannot_be_in_the_past
+
+  private
+
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date < Date.today
+      errors.add :due_date, "can't be in the past"
+    end
+  end
 end
