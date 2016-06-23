00f702c6d2ac9d0c95aa6dd7e38693402f821d18
James Dabbs <jamesdabbs@gmail.com>
2016-06-23 10:10:17 -0400

    Hook up devise User accounts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
     @username = "World"
+  end
+
+  def super_secret
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   root 'lists#index'
 
   # get "/lists/new" => "lists#new", as: :make_a_list
