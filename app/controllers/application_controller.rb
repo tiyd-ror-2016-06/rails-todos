ce103608772437b7de4e32537c2a7bca7114bbe5
James Dabbs <jamesdabbs@gmail.com>
2016-06-23 11:01:54 -0400

    Lock it down

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
   include Pundit
+  after_action :verify_authorized
 
   rescue_from Pundit::NotAuthorizedError do |e|
     flash[:danger] = "NO!"
