09816286b6b8a494b5deb3b1f20be7278b07eebd
James Dabbs <jamesdabbs@gmail.com>
2016-06-27 10:32:14 -0400

    Add click handler

--- app/assets/javascripts/hello.js
+++ app/assets/javascripts/hello.js
@@ -9,7 +9,14 @@ var styleLinks = function(btnClass) {
   $(".js-btn").addClass("btn-" + btnClass)
 }
 
+// .addEventListener("click", function())
+// .onClick = function()
 $(document).ready(function() {
   setupHeader()
   styleLinks("primary")
+
+  $("a").click(function(e) {
+    e.preventDefault()
+    alert("You clicked on a link")
+  })
 })
