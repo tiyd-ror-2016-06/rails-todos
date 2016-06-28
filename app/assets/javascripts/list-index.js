4519bbb6aae251311975e56df2c8d2c029a2b72f
James Dabbs <jamesdabbs@gmail.com>
2016-06-28 12:00:11 -0400

    Auto-update lists of lists

--- app/assets/javascripts/list-index.js
+++ app/assets/javascripts/list-index.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+var showLists = function() {
+  console.log("Checking for new lists")
 
   $.ajax("/lists.json", {
     method: "GET",
@@ -6,14 +7,20 @@ $(document).ready(function() {
     success: function(data) {
       // console.log(data)
       var floop = $("#my-lists")
+      floop.empty()
 
       for (var i=0; i < data.lists.length; i++) {
         var list = data.lists[i]
-            
+
         var newItem = $("<li>").text(list.title + " (" + list.item_count + ")")
         floop.append(newItem)
       }
     }
   })
+}
 
+$(document).ready(function() {
+  setInterval(showLists, 5000)
+
+  showLists()
 })
